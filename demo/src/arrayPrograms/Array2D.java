03c805bef8122eb56d326a14de5e310ef58ec1f8
sandeepchvn <sandeepchvn1999@gmail.com>
2026-04-19 16:07:24 +0530

    updated

--- demo/src/arrayPrograms/Array2D.java
+++ demo/src/arrayPrograms/Array2D.java
@@ -1,12 +1,15 @@
 package arrayPrograms;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Array2D {
 
 	public static void main(String[] args) {
 //		m1();
-		m2();
+//		m2();
+		m3();
+		
 	}
 	//printing 2d array
 	public static void m1() {
@@ -36,4 +39,28 @@ public class Array2D {
 			System.out.println();
 		}
 	}
+	// taking elements in 2D array
+	public static void m3() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter numbers of rows");
+		int r=sc.nextInt();
+		System.out.println("enter numbers of colums");
+		int c=sc.nextInt();
+		int[][] arr=new int[r][c];
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("enter "+arr[i].length+" elements");
+			for(int j=0;j<arr[i].length;j++) {
+				arr[i][j]=sc.nextInt();
+			}
+		}
+		print2DArray(arr);
+	}
+	public static void print2DArray(int[][] arr) {
+		//arr.length -> number of rows will be given
+		for(int r=0;r<arr.length;r++) {
+			System.out.println(Arrays.toString(arr[r]));
+			System.out.println();
+		}
+	}
+	
 }
--- demo/src/arrayPrograms/Program1.java
+++ demo/src/arrayPrograms/Program1.java
@@ -15,7 +15,14 @@ public class Program1 {
 //	m1();
 //	m2();
 //		m3();
-		m5();
+//		m5();
+//		int[] arr = { 1, 2, 3, 4 };
+//		System.out.println(Arrays.toString(arr));
+//		swap(arr, 0, 3);
+//		System.out.println(Arrays.toString(arr));
+		int[] arr = { 1, 2, 3, 4 };
+		System.out.println(binarySearch(arr, 3));
+
 	}
 
 	public static int[] reverse(int[] arr) {
@@ -178,7 +185,55 @@ public class Program1 {
 				}
 			}
 		}
+	}
+	
+	public static void swap(int[] arr ,int index1, int index2) {
+		int temp=arr[index1];
+		arr[index1]=arr[index2];
+		arr[index2]=temp;
+	}
+	
+	public static int binarySearch(int[] arr, int target) {
+		int start=0;
+		int end=arr.length-1;
+		while(start<=end) {
+			int mid=start+(end-start)/2;
+			if(target<arr[mid]) {
+				end=mid-1;
+			}else if(target > arr[mid]) {
+				start=mid+1;
+			}else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+	public static int orderBinarySearch(int[] arr, int target) {
+		int start=0;
+		int end=arr.length-1;
 		
+		boolean isAsc=arr[start] <arr[end];
+		
+		while(start<=end) {
+			int mid=start+(end-start)/2;
+			if(arr[mid]==target) {
+				return mid;
+			}
+			if(isAsc) {
+				if(target<arr[mid]) {
+					end=mid-1;
+				}else {
+					start=mid+1;
+				}
+			}else {
+				if(target>arr[mid]) {
+					end=mid-1;
+				}else {
+					start=mid+1;
+				}
+			}
+		}
+		return -1;
 	}
 }
 
